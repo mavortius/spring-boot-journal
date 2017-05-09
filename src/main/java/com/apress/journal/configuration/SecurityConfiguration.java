1867d211d4af865167d7de9b8c26fb1c25602d09
Marcelo Martins <m.a.martins@live.com>
2017-05-09 07:24:48 -0300

    Simplification of authentication config files.

--- src/main/java/com/apress/journal/configuration/SecurityConfiguration.java
+++ src/main/java/com/apress/journal/configuration/SecurityConfiguration.java
@@ -5,13 +5,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
-import org.springframework.security.config.annotation.authentication.configurers.GlobalAuthenticationConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableGlobalAuthentication
-public class GlobalSecurityConfiguration extends GlobalAuthenticationConfigurerAdapter {
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private UserDetailsService userDetailsService;
@@ -21,10 +22,23 @@ public class GlobalSecurityConfiguration extends GlobalAuthenticationConfigurerA
         return new BCryptPasswordEncoder();
     }
 
-    @Override
-    public void init(AuthenticationManagerBuilder auth) throws Exception {
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth
                 .userDetailsService(userDetailsService)
                 .passwordEncoder(passwordEncode());
     }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/**").authenticated()
+                .and()
+                .formLogin().loginPage("/login").permitAll()
+                .and()
+                .logout().permitAll();
+    }
+
+
 }
